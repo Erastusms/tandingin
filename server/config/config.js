c942d7681e5f4f05bb4ee69b32ed719704724285
Erastus Mauliate <emauliate17@gmail.com>
2023-12-12 20:51:57 +0700

    edit config database

--- server/config/config.js
+++ server/config/config.js
@@ -1,24 +1,29 @@
 require('dotenv').config()
+const pg = require('pg');
+
 module.exports = {
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: "postgres"
+    dialect: "postgres",
+    dialectModule: pg,
   },
   test: {
     username: "root",
     password: null,
     database: "database_test",
     host: "127.0.0.1",
-    dialect: "postgres"
+    dialect: "postgres",
+    dialectModule: pg,
   },
   production: {
     username: "root",
     password: null,
     database: "database_production",
     host: "127.0.0.1",
-    dialect: "postgres"
+    dialect: "postgres",
+    dialectModule: pg,
   }
 };
