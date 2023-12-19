fa5abb9024d903c0277c123e2a5d4400420f1361
Erastus Mauliate <emauliate17@gmail.com>
2023-12-19 11:00:13 +0700

    edited validation

--- server/middlewares/ValidationHelper.js
+++ server/middlewares/ValidationHelper.js
@@ -1,77 +1,77 @@
-const failActionHandler = (request, reply, source, error) => {
-
-  error.output.payload = {
+// const failActionHandler = (request, reply, source, error) => {
+const failActionHandler = (error) => {
+  error.output = {
     status: false,
     error: ''
   };
 
-  const err = error && error.data && error.data.details && error.data.details[0];
+  const err = error && error && error.details && error.details[0];
   const { context: { limit, key, valids } } = err;
 
   switch (err.type) {
     case 'object.base':
-      key === 'value' ? error.output.payload.error = 'Payload can not be empty' : error.output.payload.error = `${key} must be an object`;
+      key === 'value' ? error.output.error = 'Payload can not be empty' : error.output.payload.error = `${key} must be an object`;
       break;
     case 'any.required':
-      error.output.payload.error = err.path + ' is required';
+      error.output.error = err.path + ' is required';
       break;
     case 'date.base':
-      error.output.payload.error = err.path + ' must be valid date format';
+      error.output.error = err.path + ' must be valid date format';
       break;
     case 'number.min':
-      error.output.payload.error = err.path + ' has exceed or below the minimum value';
+      error.output.error = err.path + ' has exceed or below the minimum value';
       break;
     case 'number.max':
-      error.output.payload.error = err.path + ' has exceed the maximum value';
+      error.output.error = err.path + ' has exceed the maximum value';
       break;
     case 'number.base':
-      error.output.payload.error = err.path + ' must be valid number';
+      error.output.error = err.path + ' must be valid number';
       break;
     case 'object.missing':
-      error.output.payload.error = 'Please fill filter before search';
+      error.output.error = 'Please fill filter before search';
       break;
     case 'date.min':
-      error.output.payload.error = 'End Date must be greater or equal than Start Date';
+      error.output.error = 'End Date must be greater or equal than Start Date';
       break;
     case 'date.isoDate':
-      error.output.payload.error = err.path + ' must be valid ISO format';
+      error.output.error = err.path + ' must be valid ISO format';
       break;
     case 'string.min':
-      error.output.payload.error = `${key} length must be at least ${limit} characters`;
+      error.output.error = `${key} length must be at least ${limit} characters`;
       break;
     case 'string.max':
-      error.output.payload.error = `${key} length must be less than or equal to ${limit} characters`;
+      error.output.error = `${key} length must be less than or equal to ${limit} characters`;
       break;
     case 'any.empty':
-      error.output.payload.error = `${key} must be filled`;
+      error.output.error = `${key} must be filled`;
       break;
     case 'any.allowOnly':
-      error.output.payload.error = `${key} must be one of ${valids}`;
+      error.output.error = `${key} must be one of ${valids}`;
       break;
     case 'array.base':
-      error.output.payload.error = `${key} must be an array`;
+      error.output.error = `${key} must be an array`;
       break;
     case 'boolean.base':
-      error.output.payload.error = `${key} must be a boolean`;
+      error.output.error = `${key} must be a boolean`;
       break;
     case 'object.allowUnknown':
-      error.output.payload.error = `${key} attribute is not allowed`;
+      error.output.error = `${key} attribute is not allowed`;
       break;
     case 'any.unknown':
-      error.output.payload.error = `${key} parameter must be empty`;
+      error.output.error = `${key} parameter must be empty`;
       break;
     case 'string.alphanum':
-      error.output.payload.error = `${key} value is invalid`;
+      error.output.error = `${key} value is invalid`;
       break;
     case 'string.regex.base':
-      error.output.payload.error = `${key} value is invalid`;
+      error.output.error = `${key} value is invalid`;
       break;
     default:
       console.log(err);
-      error.output.payload.error = err;
+      error.output.error = err;
   }
-
-  reply(error);
+  
+  return error.output
 };
 
 module.exports = {
